a740621a8bc0bcfe60e36b0a2050819b11fc1ffc
harith <harith@yahoo-inc.com>
2014-09-22 10:04:44 -0700

    running liquibase update in integration tests, added related dependencies in integration module

--- integration/src/test/java/com/learner/integration/DataSetupIT.java
+++ integration/src/test/java/com/learner/integration/DataSetupIT.java
@@ -1,10 +1,21 @@
 package com.learner.integration;
 
+import java.sql.SQLException;
+
+import org.junit.Rule;
 import org.junit.Test;
 
+import com.learner.database.DataSetupTest;
+import com.learner.persistence.harness.JpaRule;
+
+import liquibase.exception.LiquibaseException;
+
 public class DataSetupIT {
+	@Rule
+	public JpaRule jpaRule = new JpaRule("unit-testing-pu");
+
 	@Test
-	public void setupData() {
-		System.out.println("Running Liquibase update");
+	public void setupData() throws SQLException, LiquibaseException {
+		DataSetupTest.runLiquibaseUpdate(jpaRule);
 	}
 }
